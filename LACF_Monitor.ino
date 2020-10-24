eb6f7499721da21454e623210ea15f520a100f1e
George Coffey <george@georgecoffey.com>
2020-10-23 21:48:58 -0700

    Inital setup of Arduino sketch

---
+++ LACF_Monitor.ino
@@ -0,0 +1,23 @@
+/*#########################################################
+ * 	Los Angeles Community Fridge - Monitoring system
+ * 
+ * 	Version 0.0.1
+ * 
+ * 
+ * 
+ * 
+ * 
+ * ##########################################################*/
+
+
+
+void setup()
+{
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+} 
